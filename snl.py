aaca5581186020d0b94f032ef23a770b847e2622
Sebastian Pina-Otey <spinaotey@gmail.com>
2018-07-09 12:12:08 +0200

    Fixed the SNL algorithm method

--- snl.py
+++ snl.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.misc import logsumexp
 from train import ConditionalTrainer
 import mcmc
+import simulators
 
 class SequentialNeuralLikelihood:
     """
@@ -18,7 +19,7 @@ class SequentialNeuralLikelihood:
         self.all_xs = None
         self.all_models = None
 
-    def learn_likelihood(self, obs_xs, model, sess, n_samples, n_rounds, train_on_all=True, thin=10, 
+    def learn_likelihood(self, obs_xs, model, trainer, sess, n_samples, n_rounds, train_on_all=True, thin=10, 
                          save_models=False, model_sufix="model",logger=sys.stdout, rng=np.random):
         """
         :param obs_xs: the observed data
@@ -37,11 +38,20 @@ class SequentialNeuralLikelihood:
         self.all_xs = []
         self.all_models = []
 
-        log_posterior = lambda t: model.eval([t, obs_xs],sess).flatten() + self.prior.eval(t)
+        log_posterior = lambda t: l_posterior_f(self.prior,model,sess,t,obs_xs)
         sampler = mcmc.SliceSampler(self.prior.gen(), log_posterior, thin=thin)
-        trainer = ConditionalTrainer(model)
+        
+        try:
+            aux = np.load(model_sufix+"data.npz")
+            self.all_ps.append(aux['ps'])
+            self.all_xs.append(aux['xs'])
+            rounds = int(aux['ps'].shape[0]/n_samples)
+            no_file = False
+        except:
+            rounds = 0
+            no_file = True
 
-        for i in range(n_rounds):
+        for i in range(rounds,n_rounds):
 
             logger.write('Learning likelihood, round {0}\n'.format(i + 1))
 
@@ -51,7 +61,7 @@ class SequentialNeuralLikelihood:
             else:
                 # MCMC sample posterior in every other round
                 logger.write('burning-in MCMC chain...\n')
-                sampler.gen(max(200 / thin, 1), logger=logger, rng=rng)  # burn in
+                sampler.gen(int(max(200 / thin, 1)), logger=logger, rng=rng)  # burn in
                 logger.write('burning-in done...\n')
                 proposal = sampler
 
@@ -73,6 +83,11 @@ class SequentialNeuralLikelihood:
             logger.write('training model...\n')
             if save_models:
                 trainer.train(sess,(ps,xs),saver_name=model_sufix+str(i),show_log=True)
+                np.savez(model_sufix+'data.npz',ps=np.concatenate(self.all_ps),xs=np.concatenate(self.all_xs))
             else:
                 trainer.train(sess,(ps,xs),show_log=True)
             logger.write('training done\n')
+
+def l_posterior_f(prior,model,sess,t,obs_xs):
+    vt = np.repeat([t],obs_xs.shape[0],axis=0)
+    return model.eval([vt, obs_xs],sess).sum() + prior.eval(t)
